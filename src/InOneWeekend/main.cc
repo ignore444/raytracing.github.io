b1ed439aa63a6cc1c3723d62a9c76297968ad8ee
ignore <_ignore_@hanmail.net>
2023-04-11 10:11:49 +0900

    [_1_4] - Rendering a blue-to-white gradient

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -87,13 +87,73 @@ hittable_list random_scene() {
     return world;
 }
 
+namespace Rendering_BlueToWhiteGradient
+{
+    color ray_color(const ray& r) {
+        vec3 unit_direction = unit_vector(r.direction());
+        auto t = 0.5 * (unit_direction.y() + 1.0);
+        return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+    }
 
-int main() {
+    void main()
+    {
+        // Image
+        const auto aspect_ratio = 16.0 / 8.0;
+        const int image_width = 10;
+        const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+        // Camera
+
+        auto viewport_height = 2.0;
+        auto viewport_width = aspect_ratio * viewport_height;
+        auto focal_length = 1.0;
+
+        auto origin = point3(0, 0, 0);
+        auto horizontal = vec3(viewport_width, 0, 0);
+        auto vertical = vec3(0, viewport_height, 0);
+        auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+        // Render
+
+        std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+        // #_1_4 : Listing 9: [main.cc] Rendering a blue-to-white gradient
+        // image 픽셀 수 만큼 ray를 만든다.
+        // U/V : [0,1]
+        // lower_left_corner : (-2,-1,-1)
+        // viewport_width , viewport_height : 4 , 2
+        // horizontal   : (4,0,0)
+        // vertical     : (0,2,0)
+        // r : o (0,0,0)
+        //     d (-2,+1,-1) , uv (0,1) 
+        //     d (+2,-1,-1) , uv (1,0)
+        // pixel_color( color ) : [0,1]
+        for (int j = image_height - 1; j >= 0; --j) 
+        {
+            std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            for (int i = 0; i < image_width; ++i) 
+            {
+                auto u = double(i) / (image_width - 1);     // 0 -> 1
+                auto v = double(j) / (image_height - 1);    // 1 -> 0
+                ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+                color pixel_color = ray_color(r);
+                write_color(std::cout, pixel_color, 1);
+            }
+        }
+
+        std::cerr << "\nDone.\n";
+    }
+};
+
+
+namespace OneWeekend
+{
+void main() {
 
     // Image
 
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 1200;
+    const int image_width = 200; //1200;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 10;
     const int max_depth = 50;
@@ -131,4 +191,9 @@ int main() {
     }
 
     std::cerr << "\nDone.\n";
+}
+};
+void main()
+{
+	return Rendering_BlueToWhiteGradient::main();
 }
